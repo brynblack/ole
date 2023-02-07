bca67c14b0c8f67901bf79ffbed792f01da12491
Brynley Llewellyn-Roux <49110391+brynblack@users.noreply.github.com>
2023-02-07 22:10:49 +1100

    Added authentication and account routes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn login() -> HttpResponse {
+    HttpResponse::Ok().body("Logged in!")
+}
+
+async fn logout() -> HttpResponse {
+    HttpResponse::Ok().body("Logged out!")
+}
+
+async fn create_acc() -> HttpResponse {
+    HttpResponse::Ok().body("Created Account!")
+}
+
+async fn remove_acc() -> HttpResponse {
+    HttpResponse::Ok().body("Deleted Account!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/api")
+                .service(
+                    web::resource("/auth")
+                        .route(web::post().to(login))
+                        .route(web::delete().to(logout)),
+                )
+                .service(
+                    web::resource("/accounts")
+                        .route(web::post().to(create_acc))
+                        .route(web::delete().to(remove_acc)),
+                ),
+        )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
