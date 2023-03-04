0b91f2e285ef2c991fe0302d92e6dae171701eaa
Brynley Llewellyn-Roux <49110391+brynblack@users.noreply.github.com>
2023-03-04 11:31:34 +1100

    Moved license and readme to project root

--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -5,18 +5,17 @@ use crate::handlers::{accounts, auth};
 
 /// Registers the API routes.
 pub fn routes(cfg: &mut ServiceConfig) {
-    cfg.service(Files::new("/static", "./static").index_file("index.html"))
-        .service(
-            web::scope("/api")
-                .service(
-                    web::resource("/accounts")
-                        .route(web::post().to(accounts::create_acc))
-                        .route(web::get().to(accounts::get_accounts)),
-                )
-                .service(
-                    web::resource("/auth")
-                        .route(web::post().to(auth::login))
-                        .route(web::delete().to(auth::logout)),
-                ),
-        );
+    cfg.service(
+        web::scope("/api")
+            .service(
+                web::resource("/accounts")
+                    .route(web::post().to(accounts::create_acc))
+                    .route(web::get().to(accounts::get_accounts)),
+            )
+            .service(
+                web::resource("/auth")
+                    .route(web::post().to(auth::login))
+                    .route(web::delete().to(auth::logout)),
+            ),
+    );
 }
