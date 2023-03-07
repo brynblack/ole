edd016a271fd8e0bb3a00ee9620c1a25f3b8adb8
Brynley Llewellyn-Roux <49110391+brynblack@users.noreply.github.com>
2023-03-07 13:43:19 +1100

    Reworked login page and made it work

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -7,7 +7,33 @@ fn main() {
 fn app(cx: Scope) -> Element {
     cx.render(rsx! {
         div {
-            "Hello, world!"
+            class: "ole",
+            style { include_str!("./assets/style.css") }
+            form {
+                class: "login-box",
+                action: "https://localhost:8081/api/auth",
+                method: "post",
+                target: "temp",
+                iframe {
+                    name: "temp",
+                    display: "none",
+                }
+                h1 { "Online Learning Environment" }
+                input {
+                    placeholder: "Username",
+                    name: "username",
+                    autofocus: "true",
+                }
+                input {
+                    placeholder: "Password",
+                    name: "password",
+                    r#type: "password",
+                }
+                input {
+                    r#type: "submit",
+                    value: "Submit",
+                }
+            }
         }
     })
 }
