b00ac76c133a22aa6f9178ee4e54a5be05bed74f
Brynley Llewellyn-Roux <49110391+brynblack@users.noreply.github.com>
2023-05-03 08:27:21 +1000

    Added resets and formatted HTML

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -16,41 +16,6 @@ enum Route {
     NotFound,
 }
 
-#[function_component(Home)]
-fn home() -> Html {
-    let token = Some("".to_string());
-    // let token: Option<String> = None;
-
-    match token {
-        Some(_) => {}
-        None => {
-            return html! {
-                <Redirect<Route> to={Route::Login}/>
-            }
-        }
-    };
-
-    html! {
-        <>
-            <div class="home">
-                <div class="dashboard">
-                    <nav>
-                        <img class="ham" alt="Overview" src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Hamburger_icon.svg"/>
-                        <img class="pfp" alt="Profile" src="https://avatars.githubusercontent.com/u/49110391?v=4"/>
-                    </nav>
-                    <h4>{ "Courses" }</h4>
-                    <div class="courses">
-                        <CourseCard id="sdd" name="Software Design Development" img="https://www.newsmaritime.com/wp-content/uploads/2016/06/Check-out-the-new-and-functional-programming-language.jpg"/>
-                        <CourseCard id="ipt" name="Information Processes Technology" img="https://www.databankimx.com/wp-content/uploads/2016/01/Database-Services-page-banner.jpg"/>
-                        <CourseCard id="coc" name="Coding Club" img="https://theaxiom.ca/wp-content/uploads/2015/03/muchbetter.jpg"/>
-                        <CourseCard id="ist" name="Information Services Technology" img="https://www.myhtptech.com/wp-content/uploads/2020/07/businessIntelligenceservices.jpg"/>
-                    </div>
-                </div>
-            </div>
-        </>
-    }
-}
-
 #[derive(Properties, PartialEq)]
 struct CardProps {
     id: String,
@@ -68,48 +33,65 @@ fn course_card(props: &CardProps) -> Html {
     }
 }
 
+#[function_component(Home)]
+fn home() -> Html {
+    html! {
+        <div class="home-page">
+            <div class="dashboard">
+                <nav>
+                    <img class="ham" alt="Overview" src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Hamburger_icon.svg"/>
+                    <img class="pfp" alt="Profile" src="https://avatars.githubusercontent.com/u/49110391?v=4"/>
+                </nav>
+                <h4>{ "Courses" }</h4>
+                <div class="courses">
+                    <CourseCard id="sdd" name="Software Design Development" img="https://www.newsmaritime.com/wp-content/uploads/2016/06/Check-out-the-new-and-functional-programming-language.jpg"/>
+                    <CourseCard id="ipt" name="Information Processes Technology" img="https://www.databankimx.com/wp-content/uploads/2016/01/Database-Services-page-banner.jpg"/>
+                    <CourseCard id="coc" name="Coding Club" img="https://theaxiom.ca/wp-content/uploads/2015/03/muchbetter.jpg"/>
+                    <CourseCard id="ist" name="Information Services Technology" img="https://www.myhtptech.com/wp-content/uploads/2020/07/businessIntelligenceservices.jpg"/>
+                </div>
+            </div>
+        </div>
+    }
+}
+
 #[function_component(Login)]
 fn login() -> Html {
     let navigator = use_navigator().unwrap();
     let onsubmit = Callback::from(move |_| navigator.push(&Route::Home));
 
     html! {
-        <>
-            <div class="login">
-                <form class="login-box" onsubmit={ onsubmit }>
-                    <span>{ "Sign In" }</span>
-                    <div class="entry">
-                        <label for="username">{ "Username" }</label>
-                        <input id="username" name="username" type="text"/>
-                    </div>
-                    <div class="entry">
-                        <label for="password">{ "Password" }</label>
-                        <input id="password" name="password" type="password"/>
-                        <a href="">{ "Forgot Password?" }</a>
-                    </div>
-                    <button class="submit-button">{ "Sign In" }</button>
-                </form>
-            </div>
-        </>
+        <div class="login-page">
+            <form class="login-box" onsubmit={ onsubmit }>
+                <span>{ "Sign In" }</span>
+                <div class="entry">
+                    <label for="username">{ "Username" }</label>
+                    <input id="username" name="username" type="text"/>
+                </div>
+                <div class="entry">
+                    <label for="password">{ "Password" }</label>
+                    <input id="password" name="password" type="password"/>
+                    <a href="">{ "Forgot Password?" }</a>
+                </div>
+                <button class="submit-button">{ "Sign In" }</button>
+            </form>
+        </div>
     }
 }
 
 #[function_component(NotFound)]
 fn not_found() -> Html {
     html! {
-        <>
-            <div class="notfound">
-                <h1>{ "The page you were looking for could not be found :(" }</h1>
-            </div>
-        </>
+        <div class="notfound-page">
+            <h1>{ "404: The page you were looking for could not be found :(" }</h1>
+        </div>
     }
 }
 
 fn switch(routes: Route) -> Html {
     match routes {
-        Route::Home => html! { <Home/> },
-        Route::Login => html! { <Login/> },
-        Route::NotFound => html! { <NotFound/> },
+        Route::Home => html! { <Home /> },
+        Route::Login => html! { <Login /> },
+        Route::NotFound => html! { <NotFound /> },
     }
 }
 
@@ -117,7 +99,7 @@ fn switch(routes: Route) -> Html {
 fn app() -> Html {
     html! {
         <BrowserRouter>
-            <Switch<Route> render={switch}/>
+            <Switch<Route> render={ switch } />
         </BrowserRouter>
     }
 }
