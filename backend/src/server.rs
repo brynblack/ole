a4a6231b85ea751e5188cf35636c578dbcf79133
Brynley Llewellyn-Roux <49110391+brynblack@users.noreply.github.com>
2023-06-30 11:12:56 +1000

    Fixed lesson rendering

--- backend/src/server.rs
+++ backend/src/server.rs
@@ -1,11 +1,13 @@
 use crate::handlers::accounts::create_acc;
+use crate::handlers::courses::create_course;
+use crate::handlers::lessons::create_lesson;
 use crate::AppState;
 use crate::{database, routes::routes};
 use actix_cors::Cors;
 use actix_web::http::header;
 use actix_web::web::Json;
 use actix_web::{web::Data, App, HttpServer};
-use common::NewAcc;
+use common::{CourseInfo, LessonInfo, NewAcc};
 use log::info;
 use std::env;
 
@@ -29,6 +31,27 @@ pub async fn run() -> std::io::Result<()> {
             username: "admin".to_string(),
             password: "password".to_string(),
             pfp: "https://upload.wikimedia.org/wikipedia/commons/b/b5/Windows_10_Default_Profile_Picture.svg".to_string(),
+        }),
+    )
+    .await;
+
+    create_course(
+        app_data.clone(),
+        Json(CourseInfo {
+            name: "Software Design Development".to_string(),
+            description: "This course provides students with the opportunity to develop skills in designing and developing software solutions, project management and communication. It does this by looking at the different ways in which software can be developed, the tools that can be used to assist in this process and by considering the interaction between software and other components of computer systems. Students apply a systematic approach to develop and document software solutions using a variety of data structures and language facilities.".to_string(),
+            image: "http://wallup.net/wp-content/uploads/2017/03/29/476208-code-web_development-JavaScript-Computer_screen-pixels-programming-PHP-syntax_highlighting-programming_language-HTML.jpg".to_string(),
+        }),
+    )
+    .await;
+
+    create_lesson(
+        app_data.clone(),
+        Json(LessonInfo {
+            reference: "software-design-development".to_string(),
+            name: "Data Structures and Algorithms".to_string(),
+            content: "A data structure is a named location that can be used to store and organize data. And, an algorithm is a collection of steps to solve a particular problem. Learning data structures and algorithms allow us to write efficient and optimized computer programs.".to_string(),
+            image: "https://blog-c7ff.kxcdn.com/blog/wp-content/uploads/2019/11/Banner-Blog-1A-1.jpg".to_string(),
         }),
     )
     .await;
